d2ffd8f6e5e518de3c568b3db03b4737a7412fab
sungwon <thesungwon@gmail.com>
2023-05-09 00:25:50 +0900

    commit 1st

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-import './globals.css'
+import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import Providers from "@/components/Providers"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +16,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={cn(
+      "bg-white text-slate-900 antialiased", inter.className
+    )}>
+      <body className="min-h-screen white:bg-slate-50 dark:gb-slate-900 antialiased">
+        <Providers>
+        {children}
+
+        <Navbar />
+        </Providers>
+
+        {/* Allow from more height on mobile devices */}
+        <div className='h-40 md:hidden'/>
+        </body>
     </html>
   )
 }
